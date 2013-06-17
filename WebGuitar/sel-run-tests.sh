f5753a43b24e60c84387f91b51b8436a066fe4d9
tirish <mafialildude@gmail.com>
2013-06-17 17:56:40 -0400

    Update sel-run-tests.sh
    Different for-loop structure

--- WebGuitar/sel-run-tests.sh
+++ WebGuitar/sel-run-tests.sh
@@ -2,7 +2,35 @@
 
 
 echo "Testing core modules"
-for dir in ./modules/*-core; do (cd "$dir"/tests && ant report); done
+for dir in ./modules/*-core; do 
+
+  if [ -d "$dir" ]; then
+    cd "$dir"
+    
+    if [-d ./tests ]; then
+      cd tests
+      
+      ant report
+    fi
+    
+  fi
+  
+done
+
+
 
 echo "Testing web modules"
-for dir in ./modules/*-sel; do (cd "$dir"/tests && ant report); done
+for dir in ./modules/*-sel; do 
+
+  if [ -d "$dir" ]; then
+    cd "$dir"
+    
+    if [-d ./tests ]; then
+      cd tests
+      
+      ant report
+    fi
+    
+  fi
+  
+done
