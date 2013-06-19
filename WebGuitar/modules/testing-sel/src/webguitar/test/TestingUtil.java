2ff11d7cfee46e9765cd7f85ffa6e388d3a56923
tirish <mafialildude@gmail.com>
2013-06-19 17:05:19 -0300

    Update TestingUtil.java

--- WebGuitar/modules/testing-sel/src/webguitar/test/TestingUtil.java
+++ WebGuitar/modules/testing-sel/src/webguitar/test/TestingUtil.java
@@ -4,9 +4,6 @@ import java.lang.reflect.Constructor;
 import java.io.File;
 import org.apache.commons.io.FilenameUtils;
 
-import static org.junit.Assert.*;
-
-import org.junit.Test;
 
 public class TestingUtil {
 	
@@ -23,12 +20,6 @@ public class TestingUtil {
 	
 	private String tcDirName = "./currentTC";
 	
-	
-	@Test
-	public void test(){
-		//load successful
-		assertTrue(true);
-	}
 	
 	public TestingUtil(String expectedDir, String website, String fileName, boolean isolate){
 		
