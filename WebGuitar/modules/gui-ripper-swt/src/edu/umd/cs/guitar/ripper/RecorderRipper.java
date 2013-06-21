f14faea64371edd3f2ff4a2b72d5f4f20a993c54
tirish <mafialildude@gmail.com>
2013-06-21 14:54:06 -0300

    Update RecorderRipper.java

--- WebGuitar/modules/gui-ripper-swt/src/edu/umd/cs/guitar/ripper/RecorderRipper.java
+++ WebGuitar/modules/gui-ripper-swt/src/edu/umd/cs/guitar/ripper/RecorderRipper.java
@@ -426,9 +426,7 @@ public class RecorderRipper extends Ripper
 
 				} catch (Exception e) {
 					GUITARLog.log.error("Uncaught exception while ripping" + e);
-					GUITARLog.log.error("Likely AUT bug. If not, file GUITAR bug");
-					GUITARLog.log.error("Stacktrace: " + e.getStackTrace().toString);//ADDED FOR DEBUG
-					
+					GUITARLog.log.error("Likely AUT bug. If not, file GUITAR bug");					
 					e.printStackTrace();
 				}
 			}
