60c5602a8abf2b6c1c2a931d77116662b4f939a2
tirish <mafialildude@gmail.com>
2013-06-24 15:39:27 -0300

    Update execute.sh

--- WebGuitar/modules/testing-sel/execute.sh
+++ WebGuitar/modules/testing-sel/execute.sh
@@ -1,8 +1,15 @@
+testcase_dir="./src/webguitar/test/"
+testcase_package="webguitar.test."
+
+guitar_opts="-Dlog4j.configuration=log/guitar-clean.glc"
 
 
 junitjar=../../lib/misc/junit.jar
 
 guitar_lib=../../dist/guitar
+
+utiljar="./src/jars/webguitar-test-util.jar"
+testjar="./src/jars/webguitar-unit-tests.jar"
 
 
 for file in `find $guitar_lib -name '*.jar'` 
@@ -10,9 +17,22 @@ do
   classpath=$classpath:$file
 done
 
-classpath=$classpath:$junitjar
+classpath=$classpath:$junitjar:$utiljar:$testjar
 
 
 
+#run test cases
 
-java -cp $classpath org.junit.runner.JUnitCore webguitar.test.HelloWorld
+for testfile in `find $testcase_dir -name '*Test.java'`
+do
+  filewithoutpath=$(basename $testfile)
+  testcase=${filewithoutpath%.*}
+  echo "Running testcase: $testcase"
+  testclass=$testcase_package$testcase
+
+  outputfile=$testcase".out"
+  errorfile=$testcase".err"
+
+
+  java $guitar_opts -cp $classpath org.junit.runner.JUnitCore $testclass > $outputfile 2> $errorfile
+done
