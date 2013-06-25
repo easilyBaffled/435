1bcb1a8e1d43a868334ed3a795656e6521471668
tirish <mafialildude@gmail.com>
2013-06-25 14:52:56 -0300

    Update HelloWorldTest.sh

--- WebGuitar/WebGuitarTesting/HelloWorldTest.sh
+++ WebGuitar/WebGuitarTesting/HelloWorldTest.sh
@@ -1,3 +1,7 @@
+#Usage:
+# HelloWorldTest.sh [isolate]
+# NOTE: Isolate argument is optional and will default to true
+
 # Prepare arguments for the testcase template script
 
 testcase_template="./Testcase-Template.sh"
@@ -5,7 +9,13 @@ testcase_template="./Testcase-Template.sh"
 
 testname="HelloWorldTest"
 website="https://googledrive.com/host/0B6TP-LuwGMgZbERwOXF5ZTNzWW8/helloworld.html"
+
+
 isolate="true"
+if [ ! -z "$1" ]; then
+  isolate=$1
+end
+
 
 #execute testcase-template
 bash $testcase_template $isolate $testname $website
