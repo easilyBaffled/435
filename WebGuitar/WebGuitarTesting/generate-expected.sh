4dae974d9f4941e0ed9894b328102e2f0cd3018f
tirish <mafialildude@gmail.com>
2013-06-25 21:04:06 -0300

    Update generate-expected.sh

--- WebGuitar/WebGuitarTesting/generate-expected.sh
+++ WebGuitar/WebGuitarTesting/generate-expected.sh
@@ -16,10 +16,12 @@ testcase_expected_dir=$expected_dir/$testname
 generated_testcase_dir="$testcase_expected_dir/TC"
 
 if [ ! -d $expected_dir ]; then
+  echo "[INFO] generate-expected($testname): Creating EXPECTED directory: $expected_dir"
   mkdir $expected_dir
 fi
 
 if [ ! -d $testcase_expected_dir ]; then
+  echo "[INFO] generate-expected($testname): Creating EXPECTED directory for this testcase"
   mkdir $testcase_expected_dir
 fi
 
@@ -42,6 +44,8 @@ ripper_args="--website-url $website -w $width -d $depth -g $output_file_path.GUI
 
 
 # run ripper
+
+echo "[INFO] - generate-expected($testname): Running ripper.."
 bash $dist_dir/sel-ripper.sh $ripper_args 
 
 
@@ -49,17 +53,20 @@ bash $dist_dir/sel-ripper.sh $ripper_args
 # run gui 2 efg
 gui_to_efg_args="-g $output_file_path.GUI -e $output_file_path.EFG"
 
+echo "[INFO] - generate-expected($testname): Running GUI-to-EFG.."
 bash $dist_dir/gui2efg.sh $gui_to_efg_args
 
 
 # run testcase generator
 tc_args="-e $output_file_path.EFG -m $max_testcases -d $generated_testcase_dir"
 
+echo "[INFO] - generate-expected($testname): Running testcase generator.."
 bash $dist_dir/tc-gen-sq.sh $tc_args
 
 
 # run replayer
 
+echo "[INFO] - generate-expected($testname): Running replayer.."
 
 for testcase in `find $generated_testcase_dir -name "*.tst"| head -n$max_testcases`  
 do
@@ -74,7 +81,7 @@ do
 	
 
 done
-
+echo "[INFO] - generate-expected($testname): Completed generation."
 
 
 
