468447dd13c755d04d2ae5708f6afa4f45b877aa
tirish <mafialildude@gmail.com>
2013-06-25 22:14:53 -0300

    Update testsuite.sh

--- WebGuitar/WebGuitarTesting/testsuite.sh
+++ WebGuitar/WebGuitarTesting/testsuite.sh
@@ -6,17 +6,17 @@ succ_count=0
 
 for testfile in `find $testcase_dir -name '*Test.sh'`
 do
-  total_count=`expr $total_count + 1`
+  let total_count+=1
   
   bash $testfile
   status=$?
   
   if [ $status -eq 0 ];then
     echo "$testfile passed!"
-    succ_count=`expr $succ_count + 1`
+    let succ_count+=1
   else
     echo "$testfile failed!"
-    error_count=`expr $error_count + 1`
+    let error_count+=1
   fi
   
 done
